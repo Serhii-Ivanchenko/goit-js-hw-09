992637ed52c5341d721f2c3e32af51e66c8da04b
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-20 15:06:57 +0200

    created structure for hw and library installed

---
+++ src/js/1-gallery.js
@@ -0,0 +1,9 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+console.log(SimpleLightbox);
